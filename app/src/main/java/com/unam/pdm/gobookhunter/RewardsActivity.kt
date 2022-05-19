3989738c5ddd5a79cf790352e823b72af43ebc87
DanyDolphin <danielcastillo59814@ciencias.unam.mx>
2022-05-19 13:21:09 -0500

    Agregada actividad RewardsActivity

---
+++ app/src/main/java/com/unam/pdm/gobookhunter/RewardsActivity.kt
@@ -0,0 +1,11 @@
+package com.unam.pdm.gobookhunter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RewardsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rewards)
+    }
+}
