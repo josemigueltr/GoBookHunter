d0847e9788d16de5ff3176e0a4e4123c0c3bd0b9
DanyDolphin <danielcastillo59814@ciencias.unam.mx>
2022-05-19 13:38:54 -0500

    Mostrado puntaje en RewardsActivity usando la clase DataPersistence

--- app/src/main/java/com/unam/pdm/gobookhunter/RewardsActivity.kt
+++ app/src/main/java/com/unam/pdm/gobookhunter/RewardsActivity.kt
@@ -2,10 +2,19 @@ package com.unam.pdm.gobookhunter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.unam.pdm.gobookhunter.utilities.DataPersistence
 
 class RewardsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rewards)
+
+        // Mostramos score guardado
+        val persistence = DataPersistence(applicationContext)
+        // persistence.save(persistence.SCORE, "100")
+        val score = Integer.parseInt(persistence.read(persistence.SCORE))
+        findViewById<TextView>(R.id.score)
+            .setText("Puntuación ${score}")
     }
 }
--- app/src/main/java/com/unam/pdm/gobookhunter/utilities/DataPersistence.kt
+++ app/src/main/java/com/unam/pdm/gobookhunter/utilities/DataPersistence.kt
@@ -4,6 +4,8 @@ import android.content.Context
 
 class DataPersistence(private val context: Context ) {
 
+    val SCORE = "score"
+
     fun save( propertyName: String, value: String ){
         val preferences = this.context.getSharedPreferences( "GoBookHunterPreferences", Context.MODE_PRIVATE)
         preferences.edit().putString(propertyName, value).apply()
