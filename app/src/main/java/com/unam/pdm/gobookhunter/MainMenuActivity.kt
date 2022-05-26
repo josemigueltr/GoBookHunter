04797770ea6768f71596170e9304f2840258b6d4
DanyDolphin <danielcastillo59814@ciencias.unam.mx>
2022-05-26 11:31:14 -0500

    probando libreria de QR en MainMenuActivity

--- app/src/main/java/com/unam/pdm/gobookhunter/MainMenuActivity.kt
+++ app/src/main/java/com/unam/pdm/gobookhunter/MainMenuActivity.kt
@@ -3,8 +3,14 @@ package com.unam.pdm.gobookhunter
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanIntentResult
+import com.journeyapps.barcodescanner.ScanOptions
 import com.unam.pdm.gobookhunter.databinding.ActivityMainBinding
+
 
 class MainMenuActivity: AppCompatActivity()  {
     private  lateinit var binding: ActivityMainBinding
@@ -16,11 +22,22 @@ class MainMenuActivity: AppCompatActivity()  {
         val recorrido= findViewById<ImageButton>(R.id.imageButton)
 
         recorrido.setOnClickListener{
-
-            Toast.makeText(this, "hola", Toast.LENGTH_SHORT).show()
+            barcodeLauncher.launch(ScanOptions())
         }
 
-
-
     }
+
+    private val barcodeLauncher: ActivityResultLauncher<ScanOptions> =
+        registerForActivityResult<ScanOptions, ScanIntentResult>(ScanContract(),
+            ActivityResultCallback { result: ScanIntentResult ->
+                if (result.contents == null) {
+                    Toast.makeText(this@MainMenuActivity, "Cancelled", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(
+                        this@MainMenuActivity,
+                        "Scanned: " + result.contents,
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            })
 }
